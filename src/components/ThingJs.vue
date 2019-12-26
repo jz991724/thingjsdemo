c918aeb601c6fe71d535cb018523ddb59f594aba
zhangjun <1548293573@qq.com>
2019-12-26 11:49:59 +0800

    1.0

---
+++ src/components/ThingJs.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <iframe src='https://www.thingjs.com/s/cc29b743194839649660a767' scrolling="no" :style="{width:'calc(100vw -20px)',height:'calc(100vh -20px)'}"></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ThingJs"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+<!--        <img alt="Vue logo" src="../assets/logo.png"/>-->
+        <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+        <ThingJs></ThingJs>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+    import {Vue, Component} from "vue-property-decorator";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+    import ThingJs from "@/components/ThingJs.vue";
+
+    @Component({
+        components: {
+            ThingJs
+        }
+    })
+    export default class Home extends Vue{
+        name: string = "home"
+    };
 </script>
